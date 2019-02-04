67fdc3cbb8a822884cb67f70579661d5555db31c
Matthew Craig <taggedzi.mpc@gmail.com>
2019-02-03 22:15:42 -0500

    Added Call to Sphinx for documentation

--- setup.py
+++ setup.py
@@ -156,8 +156,8 @@ setup(
     # Similar to `install_requires` above, these must be valid existing
     # projects.
     extras_require={  # Optional
-        'dev': ['check-manifest', 'coverage', 'pytest', 'tox'],
-        'test': ['coverage', 'pytest', 'tox'],
+        'dev': ['check-manifest', 'coverage', 'pytest', 'Sphinx', 'tox'],
+        'test': ['coverage', 'pytest', 'Sphinx', 'tox'],
     },
 
     # If there are data files included in your packages that need to be
